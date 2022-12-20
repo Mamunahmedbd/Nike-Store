cd7d8ba81fbdce96021302e68aa68a319ab41675
Mamun Ahmed <mamunahmedusa@gmail.com>
2022-12-20 17:04:56 +0600

    This is nike store version 1.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import Store from "./app/Store.js";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
